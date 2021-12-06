0b9e4b9d4a43404b426cd68a2d311bcaab931ef8
Mayur Avinash Khandetod <khandetod.m@northeastern.edu>
2021-12-06 02:35:19 -0500

    Create procedure for delete entity & create tables

---
+++ DELETE_ENTITY.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE PROCEDURE DELETE_ENTITY(ENTNAME varchar2,ENTTYPE varchar2) IS 
+nCount NUMBER;
+BEGIN
+if ENTTYPE = 'TABLE' THEN
+SELECT count(*) into nCount FROM user_tables where table_name = upper(ENTNAME);
+IF (nCount > 0) THEN 
+    EXECUTE IMMEDIATE 'drop table ' || ENTNAME || ' cascade constraints';
+END IF;
+END IF;
+END DELETE_ENTITY;
